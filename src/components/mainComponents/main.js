f40426a9dc5a70ea98e412b4fcb50006583815cb
Lorel <lorel.oboue@gmail.com>
2022-07-11 17:30:33 +0100

    multiple components added and stylesheet broken down

---
+++ src/components/mainComponents/main.js
@@ -0,0 +1,12 @@
+import NavEmailToolbar from "./navEmailToolbar";
+import Article from "./article";
+
+function Main() {
+  return (
+    <main className="email-view">
+      <NavEmailToolbar />
+      <Article />
+    </main>
+  );
+}
+export default Main;
